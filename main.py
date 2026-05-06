1674b2c923f650888b5e23c5e2947aac8087b23f
Prathamesh Bhurke <prathameshbhurke666@gmail.com>
2026-05-06 22:35:50 +0530

    Sitemap.xml route added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'   # force CPU, no GPU warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'   # suppress INFO, WARNING, ERROR logs
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from routes.detect import detect_route
 
 app = Flask(__name__)
@@ -20,3 +20,7 @@ if __name__ == '__main__':
     app.run(host="0.0.0.0", port=port, debug=False)
     #app.run(host="0.0.0.0", port=443, ssl_context=("/etc/ssl/certs/dna-scanner.crt", "/etc/ssl/private/dna-scanner.key"))
     #app.run(debug=True)
+    
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.dirname(__file__), 'sitemap.xml')
